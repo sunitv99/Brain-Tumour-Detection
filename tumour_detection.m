1c21f217db896e5828422e2c20bce7f86ad620b3
sunitv99 <46061197+sunitv99@users.noreply.github.com>
2021-03-07 18:00:54 +0530

    Update and rename ipproj.m to tumour_detection.m

--- tumour_detection.m
+++ tumour_detection.m
@@ -1,8 +1,8 @@
-I=imread('C:\Users\Naren Adithya\Desktop\5.jpg');
+I=imread('4.jpg');
 figure, imshow(I); title('Brain MRI Image');
 I = imresize(I,[200,200]);
 I= rgb2gray(I);
-I= im2bw(I,.6);%binarising with thresold .6
+I= im2bw(I,.6);
 figure, imshow(I);title('Thresholded Image');
 hy = fspecial('sobel');
 hx = hy';
@@ -13,19 +13,16 @@ L = watershed(gradmag);
 Lrgb = label2rgb(L);
 figure, imshow(Lrgb), title('Watershed segmented image ')
 se = strel('disk', 20);
-Io = imopen(I, se);
 Ie = imerode(I, se);
+figure,imshow(Ie)
 Iobr = imreconstruct(Ie, I);
+figure,imshow(Iobr)
 Iobrd = imdilate(Iobr, se);
+figure,imshow(Iobrd)
 Iobrcbr = imreconstruct(imcomplement(Iobrd), imcomplement(Iobr));
+figure,imshow(Iobrcbr)
 Iobrcbr = imcomplement(Iobrcbr);
-I2 = I;
-fgm = imregionalmax(Iobrcbr);
-I2(fgm) = 255;
-se2 = strel(ones(5,5));
-fgm2 = imclose(fgm, se2);
-fgm3 = imerode(fgm2, se2);
-fgm4 = bwareaopen(fgm3, 20);
+figure,imshow(Iobrcbr)
 I3 = I;
 bw = im2bw(Iobrcbr);
 figure
